2519d20980b0793e18917a391bf2f103bd6f9fb2
zhanadil_uulu_a <zhanadil_uulu_a@iuca.kg>
2023-05-06 15:23:49 +0600

    done some parts 6 рабочий скругление углов

--- app/src/main/java/com/example/englishzone/QuizUnitActivity.java
+++ app/src/main/java/com/example/englishzone/QuizUnitActivity.java
@@ -1,10 +1,19 @@
 package com.example.englishzone;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.app.PendingIntent.getActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ScaleDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class QuizUnitActivity extends AppCompatActivity {
 
@@ -17,4 +26,10 @@ public class QuizUnitActivity extends AppCompatActivity {
         Intent intent = new Intent(QuizUnitActivity.this, MainActivity.class);
         startActivity(intent);
     }
+    public void Click_Test(View v) {
+        Intent intent = new Intent(QuizUnitActivity.this, SettingActivity.class);
+        startActivity(intent);
+    }
+
+
 }
